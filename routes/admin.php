3644d4a11b5870efffada359ded4d06219833101
MatsuriGuide <cedrick@perron.biz>
2026-08-07 13:22:37 +0200

    Add files via upload

--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,8 @@ $router->get('/admin', 'Admin\DashboardController@index');
 $router->get('/admin/tags', 'Admin\TagAdminController@index');
 $router->get('/admin/reports', 'Admin\ReportController@index');
 $router->post('/admin/reports/{id}/resolve', 'Admin\ReportController@resolve');
+$router->post('/admin/translate/artist/{id}/{lang}', 'Admin\TranslationController@translateArtist');
+$router->post('/admin/translate/video/{id}/{lang}', 'Admin\TranslationController@translateVideo');
 
 // À compléter au fur et à mesure :
 // $router->get('/admin/users', 'Admin\UserAdminController@index');
