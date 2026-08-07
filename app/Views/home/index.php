3ed405c604856df05f41314d623dda0d7223d166
MatsuriGuide <cedrick@perron.biz>
2026-08-07 10:58:21 +0200

    Add files via upload

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -6,10 +6,16 @@
     <ul>
         <?php foreach ($latestVideos as $video): ?>
             <li>
-                <a href="https://youtube.com/watch?v=<?= e($video['youtube_id']) ?>" target="_blank" rel="noopener">
-                    <?= e($video['youtube_id']) ?>
+                <a href="<?= url('/videos/' . $video['id']) ?>">
+                    <?= e($video['title'] ?? $video['youtube_id']) ?>
                 </a>
-                — <?= e($video['release_date']) ?>
+                <?php if (!empty($video['artist_names'])): ?>
+                    — <?= e($video['artist_names']) ?>
+                <?php endif; ?>
+                <?php if (!empty($video['release_date'])): ?>
+                    — <?= e($video['release_date']) ?>
+                <?php endif; ?>
+                (<a href="https://youtube.com/watch?v=<?= e($video['youtube_id']) ?>" target="_blank" rel="noopener">YouTube</a>)
             </li>
         <?php endforeach; ?>
     </ul>
