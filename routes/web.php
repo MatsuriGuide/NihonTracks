5d28f57f92f6a2e029c9876225f7d9b1e8d02667
MatsuriGuide <cedrick@perron.biz>
2026-08-07 10:06:14 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@
 
 $router->get('/', 'HomeController@index');
 
+$router->get('/register', 'AuthController@registerForm');
+$router->post('/register', 'AuthController@register');
+$router->get('/login', 'AuthController@loginForm');
+$router->post('/login', 'AuthController@login');
+$router->get('/logout', 'AuthController@logout');
+
 // À compléter au fur et à mesure :
 // $router->get('/artists', 'ArtistController@index');
 // $router->get('/artists/{slug}', 'ArtistController@show');
 // $router->get('/videos', 'VideoController@index');
 // $router->get('/videos/add', 'VideoController@create');
 // $router->post('/videos/add', 'VideoController@store');
-// $router->get('/login', 'AuthController@loginForm');
-// $router->post('/login', 'AuthController@login');
-// $router->get('/logout', 'AuthController@logout');
